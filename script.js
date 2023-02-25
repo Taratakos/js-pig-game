3a329aa5f86e4a2783a5a16247bf1ba1c34721fe
Taras Zarytskyi <taras.zarytskyi@gmail.com>
2023-02-25 23:33:09 +0200

    optimizes visual code reading 2

--- script.js
+++ script.js
@@ -4,127 +4,55 @@
 // const elemsSeven = document.querySelectorAll('#listItem'); // в такому випадку ми отримаємо лише один об'єкт якщо він є на сторінці
 
 // Selecting elements
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const diceEl = document.querySelector('.dice');
-
-// Setting initial conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-// diceEl.textContent;
-
-/*
-// Selecting elements
-const score0El = document.querySelector('#score--0');
-//  other way of selecting elements that only works by ID.
-const score1El = document.getElementById('score--1');
-// now we can just add that class to the dice at the beginning of the game. but first we need to select it.
-const diceEl = document.querySelector('.dice');
-
-// let's now set these initial conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-// Hide this dice here (creating a hidden class and then add that hidden class here at the beginning)
-diceEl.classList.add('hidden');
-*/
-// 83. Rolling the Dice
-/*
-// Selecting elements
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const currentEl1 = document.getElementById('current--1');
-
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnКRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
-
-// let's now set these initial conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-
-// Rolling dice functionality
-addEventListener('click', function () {
-  // 1. Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  // 2. Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
-  // 3. Check fo rolled 1: if true, switch to next player
-  if (dice !== 1) {
-    // add dice to the current score
-    currentScore += dice; // currentScore = currentScore + dice;
-    document.getElementById('current--$(activePlayer)').textContent =
-      currentScore; // but here we do instead select the score element dynamically based on which is the active player right now
-    current0El.textContent = currentScore; // CHANGE LATER (So again, this 1 here will always set the text content on player number 0,)
-  } else {
-    // switch to next player
-  }
-});
-*/
-//
-//
-//
-//
-//
-//
-//
-//
-// 84. Switching the Active player
-// Selecting elements
-/*
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
-const currentEl1 = document.getElementById('current--1');
+const current1El = document.getElementById('current--1');
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
-const btnКRoll = document.querySelector('.btn--roll');
+const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// let's now set these initial conditions
+// Setting initial conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+const scores = [0, 0]; // big scores which accumulate
+let currentScore = 0; // this can not be inside of this function because then it would be set to zero each time that we clicked the button
+let activePlayer = 0; // Score of 1-st player is at index 0, 2-nd index 1 in array
 
 // Rolling dice functionality
-addEventListener('click', function () {
+btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
   // 2. Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
-  // 3. Check fo rolled 1: if true, switch to next player
+
+  // 3. Check for rolled 1
   if (dice !== 1) {
-    // add dice to the current score
-    currentScore += dice; // currentScore = currentScore + dice;
+    // Add dice to the current score
+    // currentScore = currentScore + dice;
+    currentScore += dice;
     document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore; // here we do select the score element dynamically based on which is the active player right now
+      currentScore;
   } else {
-    // switch to next player
+    // Switch to next player
+    // before switching set the current score back to 0
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    ///If we do that on both then it will work, because on player 0 it will remove the class if it's there and if it's not it will add it. The same thing on the other player.
+    activePlayer = activePlayer === 0 ? 1 : 0; // then we want a new activePlayer to be 1
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
   }
 });
-*/
+
 // 85. Holding Current Score
 /*
 const player0El = document.querySelector('.player--0');
