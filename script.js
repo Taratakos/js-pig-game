7809cc8c3b6af16ceea74f5933a62b1d5111fed0
Taras Zarytskyi <taras.zarytskyi@gmail.com>
2023-02-26 04:10:32 +0200

    implements the feature of a player holding the current score

--- script.js
+++ script.js
@@ -24,84 +24,37 @@ diceEl.classList.add('hidden');
 const scores = [0, 0]; // big scores which accumulate
 let currentScore = 0; // this can not be inside of this function because then it would be set to zero each time that we clicked the button
 let activePlayer = 0; // Score of 1-st player is at index 0, 2-nd index 1 in array
-
-// Rolling dice functionality
-btnRoll.addEventListener('click', function () {
-  // 1. Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
-
-  // 2. Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
-
-  // 3. Check for rolled 1
-  if (dice !== 1) {
-    // Add dice to the current score
-    // currentScore = currentScore + dice;
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Switch to next player
-    // before switching set the current score back to 0
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0; // then we want a new activePlayer to be 1
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
-  }
-});
-
-// 85. Holding Current Score
-/*
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const currentEl1 = document.getElementById('current--1');
-
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
-
-// let's now set these initial conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let playing = true; // state variable, that hosts the state of the game
 
 const switchPlayer = function () {
+  // before switching set the current score back to 0
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
+  activePlayer = activePlayer === 0 ? 1 : 0; // then we want a new activePlayer to be 1
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   if (playing) {
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
     // 2. Display dice
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
 
-    // 3. Check fo rolled 1: if true, switch to next player
+    // 3. Check for rolled 1
     if (dice !== 1) {
-      // add dice to the current score
-      currentScore += dice; // currentScore = currentScore + dice;
+      // Add dice to the current score
+      // currentScore = currentScore + dice;
+      currentScore += dice;
       document.getElementById(`current--${activePlayer}`).textContent =
-        currentScore; // here we do select the score element dynamically based on which is the active player right now
+        currentScore;
     } else {
-      // switch to next player
+      // Switch to next player
       switchPlayer();
     }
   }
@@ -109,37 +62,29 @@ btnRoll.addEventListener('click', function () {
 
 btnHold.addEventListener('click', function () {
   if (playing) {
-    // 1. Add current score to active player's score
-    scores[activePlayer] += currentScore; // or: scores[activePlayer] + currentScore;
-    // scores[1] = scores[1] + currentScore;
+    // 1. Add current score to active players's score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-
-    //  2. Check if player's score is >= 100
+    // 2. Check if player's score is >= 100
     if (scores[activePlayer] >= 20) {
       // Finish the game
+
       playing = false;
       diceEl.classList.add('hidden');
-
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.remove('player-active');
+        .classList.remove('player--active');
     } else {
-      // Switch to next player. Code will be same but i will put it into a function
+      // Switch to the next player
       switchPlayer();
     }
   }
 });
-*/
-
-//
-//
-//
-//
-//
 
 // 86. Resetting the Game
 /*
